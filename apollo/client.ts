adfd86691d84222ff50370038ea36b8027d96803
Eduard Kluchnikov <klyuchnikov.fortech@gmail.com>
2023-02-09 13:35:39 +0300

    fixes: hide token in env

--- apollo/client.ts
+++ apollo/client.ts
@@ -4,8 +4,9 @@ const client = new ApolloClient({
   ssrMode: true,
   uri: "https://api.github.com/graphql",
   cache: new InMemoryCache(),
+  connectToDevTools:true,
   headers: {
-    Authorization: "bearer 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",
+    authorization: `bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
   },
 });
 
